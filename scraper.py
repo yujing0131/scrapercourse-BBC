f1f91cc3effb40c284b93599bff9baff834b613b
yujing0131 <snake830131@gmail.com>
2023-08-15 17:07:09 +0800

    3.5批次爬取多分頁的網頁資料

--- scraper.py
+++ scraper.py
@@ -30,4 +30,30 @@ for url in urls:
     #url_list.append(url.get('href'))
 
 
-print(tag_list)
+#print(tag_list)
+##讀取不同分頁下的內容
+for page in range(1,4):
+    response = requests.get(f"https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt?page={page}")
+
+    soup = BeautifulSoup(response.text,'lxml')
+
+    titles = soup.find_all(
+        'a',{'class':'focusIndicatorDisplayBlock bbc-uk8dsi e1d658bg0'})
+    
+    title_list = []
+    for title in titles:
+        title_list.append(title.getText())
+    
+    urls = soup.find_all(
+        'a',{'class':'focusIndicatorDisplayBlock bbc-uk8dsi e1d658bg0'})
+    tag_list = []
+    for url in urls:
+        sub_response = requests.get(url.get('href'))
+        sub_soup = BeautifulSoup(sub_response.text,'lxml')
+        tags = sub_soup.find_all('li',{'class','bbc-1msyfg1 e2o6ii40'})
+        for tag in tags:
+      
+            tag_list.append(tag.getText())
+    print(f"第{page}頁")
+    print(title_list)
+    print(tag_list)
