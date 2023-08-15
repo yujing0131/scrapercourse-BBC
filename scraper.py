fa10c15799ba0b385aa18ea092cff6e87c633afa
yujing0131 <snake830131@gmail.com>
2023-08-15 09:29:49 +0800

    #作業2:爬取BBC網站國際新聞邊提

--- scraper.py
+++ scraper.py
@@ -1,8 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt")
-print(response.text.find('尼日爾軍事政變：你需要知道的幾件事'))
+response = requests.get("https://www.bbc.com/zhongwen/trad/topics/c83plve5vmjt")
 
 soup = BeautifulSoup(response.text,'lxml')
 titles = soup.find_all(
