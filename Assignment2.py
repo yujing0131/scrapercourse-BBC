b66eb42978fe0fd172d4d4edd2ab8901992753c7
yujing0131 <snake830131@gmail.com>
2023-08-15 14:13:53 +0800

    3.3批次爬靜態網頁資料

--- Assignment2.py
+++ Assignment2.py
@@ -1,12 +1,11 @@
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get("https://www.bbc.com/zhongwen/trad/topics/cq8nqywy37yt")
+response = requests.get("https://www.bbc.com/zhongwen/trad/topics/c83plve5vmjt")
 
 soup = BeautifulSoup(response.text,'lxml')
 titles = soup.find_all(
     'a',{'class':'focusIndicatorDisplayBlock bbc-uk8dsi e1d658bg0'})
-print(titles)
 title_list = []
 for title in titles:
     title_list.append(title.getText())
